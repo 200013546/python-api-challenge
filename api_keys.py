46c544b823d0b57070d8ac8d428608edd6192470
Alan McCabe <alan.mccabe@ge.com>
2020-04-25 21:49:21 -0400

    pushing completed code

--- api_keys.py
+++ api_keys.py
@@ -1,6 +1,5 @@
 # OpenWeatherMap API Key
-# weather_api_key = "92ff904996bf0620c63ce50f42eb4c93"
-weather_api_key = "8bb4902534e8ff56223facbbe3b7bb1f"
+weather_api_key = "92ff904996bf0620c63ce50f42eb4c93"
 
 
 # Google API Key
